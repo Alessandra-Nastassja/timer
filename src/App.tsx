258df2949d7e184388f590056c4c329f9ecfc3cb
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-09-12 21:54:58 -0300

    feat: undestand styled compoments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { ThemeProvider } from "styled-components";
+
 import { Button } from "./components/Button";
+
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
-      <Button color="primary" />
-      <Button color="secondary" />
-      <Button color="danger" />
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
