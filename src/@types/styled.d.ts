6a76ab430baf61eb405c8635d95976cda7f59082
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-09-13 21:00:32 -0300

    feat? working styles global in styled components

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default';
+
+type ThemeType = defaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
