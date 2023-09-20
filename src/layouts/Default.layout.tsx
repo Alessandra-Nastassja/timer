189259ab563f1518bd1a15ea90e1cd8a7dd89fd5
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2023-09-19 21:10:51 -0300

    feat: set default layout

--- src/layouts/Default.layout.tsx
+++ src/layouts/Default.layout.tsx
@@ -1,3 +1,11 @@
-export function DefaultLayou(){
-  
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/Header";
+
+export function DefaultLayout(){
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
 }
